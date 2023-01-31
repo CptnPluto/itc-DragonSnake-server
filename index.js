4972c8efa213572df64ccb7c8e2c7eb27cb7f8b7
Avraham Schochet <avrahamschochet@Avrahams-MacBook-Air.local>
2023-01-31 14:28:43 +0200

    set up socket

--- index.js
+++ index.js
@@ -7,20 +7,45 @@ const UsersRoute = require("./routes/users_routes");
 const ScoresRoute = require("./routes/scores_routes");
 
 const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
 const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: "http://localhost:3000",
     credentials: true,
   })
 );
+
+// Access to XMLHttpRequest at 'http://localhost:8080/socket.io/?EIO=4&transport=polling&t=OO7je4q' from origin 'http://localhost:3000' has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource.
+
 app.use(cookieParser());
 app.use(morgan("tiny"));
 app.use("/users", UsersRoute);
 app.use("/scores", ScoresRoute);
 
-app.listen(PORT, () => {
+io.on("connection", (socket) => {
+  console.log("connected to socket");
+
+  socket.on("click", (data) => {
+    console.log(data);
+    socket.emit("clicked", "second");
+  });
+
+  socket.on("clicked", (data) => {
+    console.log("second event", data);
+  });
+});
+
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
