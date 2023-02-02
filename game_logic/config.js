9b4a27336d1b58d93333be9477b7fa19105d4e20
CptnPluto <cptnpluto@gmail.com>
2023-02-02 09:32:39 +0200

    Changed speed from 500 to 200

--- game_logic/config.js
+++ game_logic/config.js
@@ -2,33 +2,33 @@ const ROWS = 25;
 const COLS = 30;
 // BOARD SIZE (px) IN Game.css
 const INITIAL_DIRECTION = "RIGHT";
-const INITIAL_SPEED = 500;
+const INITIAL_SPEED = 200;
 const INITIAL_SPEED_INCREASE = 100;
 const FPS = 60;
 const cells = [];
 
 for (let i = 0; i < ROWS; i++) {
-  for (let j = 0; j < COLS; j++) {
-    cells.push({
-      row: i,
-      col: j,
-      isHead: false,
-      isTail: false,
-      isFood: false,
-    });
-  }
+    for (let j = 0; j < COLS; j++) {
+        cells.push({
+            row: i,
+            col: j,
+            isHead: false,
+            isTail: false,
+            isFood: false,
+        });
+    }
 }
 
 const INITIAL_EMPTY_BOARD = {
-  rows: ROWS,
-  cols: COLS,
-  cells: cells,
+    rows: ROWS,
+    cols: COLS,
+    cells: cells,
 };
 
 module.exports = {
-  INITIAL_DIRECTION,
-  INITIAL_SPEED,
-  INITIAL_SPEED_INCREASE,
-  FPS,
-  INITIAL_EMPTY_BOARD,
+    INITIAL_DIRECTION,
+    INITIAL_SPEED,
+    INITIAL_SPEED_INCREASE,
+    FPS,
+    INITIAL_EMPTY_BOARD,
 };
